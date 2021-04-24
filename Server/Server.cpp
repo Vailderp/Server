a1942fa2f0dcfd9bac78e517822bbbdc4da8ffc9
Vailderp <59770659+Vailderp@users.noreply.github.com>
2021-04-24 20:55:10 +0700

    v0.5

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,13 +2,21 @@
 
 int main(int argc, char* argv[])
 {
-	vl::Server server(1 << 16, 1111);
+	vl::Server server(1 << 16, 1111, "192.168.0.105");
 	
 	server.onAny([&](const char* packet, const SOCKET socket)
 		{
 		
 			server.emit(packet, socket);
 		
+		});
+
+	server.on("S",
+		[&](const char* packet, const SOCKET socket)
+		{
+			std::cout << "S" << std::endl;
+			//server.emit(packet, socket);
+
 		});
 	
 	server.start();
